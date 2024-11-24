c9d7bbb51a2e6a99d61b85b305468e4d9ba150b5
rames-D-shah <ramesh.shah@dresslerconsulting.com>
2024-11-24 16:51:36 +0545

    added test for validating auto mapper

---
+++ tests/Application.UnitTests/Common/Mappings/DomainMappingTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using AutoMapper;
+using JobCandidate.Application.Candidates.Commands.UpdateCandidate;
+using JobCandidate.Application.Common.Interfaces;
+using JobCandidate.Domain.Entities;
+using NUnit.Framework;
+
+namespace JobCandidate.Application.UnitTests.Common.Mappings;
+public class DomainMappingTests
+{
+    private readonly IConfigurationProvider _configuration;
+    private readonly IMapper _mapper;
+
+    public DomainMappingTests()
+    {
+        _configuration = new MapperConfiguration(config =>
+            config.AddMaps(Assembly.GetAssembly(typeof(IApplicationDbContext))));
+
+        _mapper = _configuration.CreateMapper();
+    }
+
+    [Test]
+    public void ShouldHaveValidConfiguration()
+    {
+        _configuration.AssertConfigurationIsValid();
+    }
+
+    [Test]
+    [TestCase(typeof(UpdateCandidateCommand), typeof(Candidate))]
+    public void ShouldSupportMappingFromSourceToDestination(Type source, Type destination)
+    {
+        var instance = GetInstanceOf(source);
+
+        _mapper.Map(instance, source, destination);
+    }
+
+    private object GetInstanceOf(Type type)
+    {
+        if (type.GetConstructor(Type.EmptyTypes) != null)
+            return Activator.CreateInstance(type)!;
+
+        // Type without parameterless constructor
+        return RuntimeHelpers.GetUninitializedObject(type);
+    }
+}
